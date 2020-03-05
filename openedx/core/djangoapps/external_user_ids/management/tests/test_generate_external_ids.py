4281d3510c56256bba69d26ad74c8b6187c96886
Albert (AJ) St. Aubin <astaubin@edx.org>
2020-03-05 11:00:15 -0500

    tests

--- openedx/core/djangoapps/external_user_ids/management/tests/test_generate_external_ids.py
+++ openedx/core/djangoapps/external_user_ids/management/tests/test_generate_external_ids.py
@@ -2,17 +2,23 @@
 Test the Generate External Ids Management command.
 """
 
+from django.conf import settings
 from django.core.management import call_command
 from django.test import TestCase
 from student.tests.factories import UserFactory
 
-from openedx.core.djangoapps.external_user_ids.models import (
-    ExternalId,
-    GenerateExternalIdsConfig
-)
-from openedx.core.djangoapps.external_user_ids.tests.factories import ExternalIDTypeFactory
+from openedx.core.djangolib.testing.utils import skip_unless_lms
+
+# external_ids is not in CMS' INSTALLED_APPS so these imports will error during test collection
+if settings.ROOT_URLCONF == 'lms.urls':
+    from openedx.core.djangoapps.external_user_ids.models import (
+        ExternalId,
+        GenerateExternalIdsConfig
+    )
+    from openedx.core.djangoapps.external_user_ids.tests.factories import ExternalIDTypeFactory
 
 
+@skip_unless_lms
 class TestGenerateExternalIds(TestCase):
     """
     Test generating ExternalIDs for Users.
@@ -45,5 +51,16 @@ class TestGenerateExternalIds(TestCase):
             )
 
         assert ExternalId.objects.count() == 10
+        call_command('generate_external_ids')
+        assert ExternalId.objects.count() == 10
+
+    def test_bad_input_does_not_break(self):
+        user_id_list_bad = self.user_id_list + ['abc', ' ', '\n']
+        GenerateExternalIdsConfig.objects.create(
+            user_list=','.join(user_id_list_bad),
+            external_id_type=ExternalIDTypeFactory.create(),
+        )
+
+        assert ExternalId.objects.count() == 0
         call_command('generate_external_ids')
         assert ExternalId.objects.count() == 10
